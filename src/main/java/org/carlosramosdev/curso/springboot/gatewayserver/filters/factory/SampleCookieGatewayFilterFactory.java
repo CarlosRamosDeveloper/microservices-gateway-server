80e0a965fdad34c131703592a6a2ac1a77055e21
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-12-17 18:24:25 +0100

    implemented apply method in SampleCookieGatewayFilterFactory

--- src/main/java/org/carlosramosdev/curso/springboot/gatewayserver/filters/factory/SampleCookieGatewayFilterFactory.java
+++ src/main/java/org/carlosramosdev/curso/springboot/gatewayserver/filters/factory/SampleCookieGatewayFilterFactory.java
@@ -1,20 +1,30 @@
 package org.carlosramosdev.curso.springboot.gatewayserver.filters.factory;
 
+import org.carlosramosdev.curso.springboot.gatewayserver.filters.SampleGlobalFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.io.ObjectInputFilter;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @Component
 public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
 
-    @Override
-    public GatewayFilter apply(ConfigurationCookie configurationCookie) {
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+    private final Logger logger = Logger.getLogger(String.valueOf(SampleCookieGatewayFilterFactory.class));
 
+    @Override
+    public GatewayFilter apply(ConfigurationCookie config) {
+        return (exchange, chain) -> {
+            logger.info("Pre gateway filter factory "+config.message);
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                Optional.ofNullable(config.value).ifPresent(cookie -> {
+                    exchange.getResponse().addCookie(ResponseCookie.from(config.name, cookie).build());
+                });
+                logger.info("Post gateway filter factory "+config.message);
             }));
         };
     }
